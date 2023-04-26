d1d9a2fcfe632f3516d1f1cd3b342312fb42d896
Rohit H Solanki <solanki.rohit6105@gmail.com>
2023-04-26 10:32:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import tkinter as tk
 from tkinter import messagebox
 
 def win(winner):
-# Create a Tkinter window
+	# Create a Tkinter window
 	root = tk.Tk()
-# Hide the main window
+	# Hide the main window
 	root.withdraw()
-# Display the message box
+	# Display the message box
 	messagebox.showinfo("Winner",winner,)
-# Destroy the Tkinter window
+	# Destroy the Tkinter window
 	root.destroy()
 
 
@@ -49,15 +49,12 @@ if __name__ == '__main__':
 				if event.button == 1:
 					board.handle_click(mx, my)
 		if board.is_in_checkmate('black'): # If black is in checkmate
-			# text = font.render('White wins!', True)
-			# screen.blit(font.render('White wins!', True,(255,244,200)), (20, 50))
 			win("White wins!")
-			pygame.display.update()
 			print('White wins!')
 			running = False
 		elif board.is_in_checkmate('white'): # If white is in checkmate
-			win("Black wins!")
 			print('Black wins!')
+			win("Black wins!")
 			running = False
 		# Draw the board
 		draw(screen)
